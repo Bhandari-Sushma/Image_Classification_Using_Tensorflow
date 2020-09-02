7eb31b074a06f4fa6b9c874bea8cdc28d2ab86b5
bhand <sushbhand123@gmail.com>
2020-09-02 22:39:28 +0200

    Moved Net.py and training_and_evaluatio.py into a new directory

---
+++ SimpleCNN/Net.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+
+# Neural network with conv and pooling layer
+model = tf.keras.Sequential(
+    [
+        tf.keras.Input(shape=(32, 32, 3)),
+        tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'),
+        tf.keras.layers.MaxPool2D(pool_size=2),
+        tf.keras.layers.Conv2D(64, 3, activation='relu'),
+        tf.keras.layers.MaxPool2D(),
+        tf.keras.layers.Conv2D(128, 3, activation='relu'),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(10, ),
+
+    ]
+)
+
+
+model.compile(
+
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+    metrics=['accuracy'],
+
+)
+
+print("this is a model")
---
+++ SimpleCNN/train_and_evaluate.py
@@ -0,0 +1,26 @@
+from SimpleCNN.Net import *
+from tensorflow.keras.datasets import cifar10
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+# import data -> cifar10 dataset is used
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# train the model
+print("Training the model: ")
+model.fit(x_train, y_train, epochs=5)
+
+# Save the entire model as a SavedModel.
+print("Saving the model")
+model.save('saved_model/my_model')
+
+
+# train the model
+print("Evaluating the model: ")
+model.evaluate(x_test, y_test)
+
+
